4b7c8e8648c110bf5fa2f6c7878d0ca1876e7264
Samantha Bracellari <sbracellari@gmail.com>
2025-12-08 22:19:29 -0500

    day 8

--- util.py
+++ util.py
@@ -2,3 +2,6 @@ def read_file(path: str) -> str:
   with open(path, "r") as file:
     data: str = file.read()
     return data
+
+def distance(a: list[int], b: list[int]) -> int:
+  return (b[0]-a[0])**2 + (b[1]-a[1])**2 + (b[2]-a[2])**2
